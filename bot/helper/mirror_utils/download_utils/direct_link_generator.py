a495e363ace91df7bbb48d08216e6b2e59f5fc6f
nb <azorax@pm.me>
2024-03-04 18:40:26 +0700

    Update direct_link_generator.py

--- bot/helper/mirror_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_utils/download_utils/direct_link_generator.py
@@ -163,7 +163,7 @@ def direct_link_generator(link):
         return streamvid(link)
     elif any(x in domain for x in ['dood.watch', 'doodstream.com', 'dood.to', 'dood.so', 'dood.cx', 'dood.la', 'dood.ws', 'dood.sh', 'doodstream.co', 'dood.pm', 'dood.wf', 'dood.re', 'dood.video', 'dooood.com', 'dood.yt', 'doods.yt', 'dood.stream', 'doods.pro', 'ds2play.com', 'dood.boo', 'dood.zip', 'poop.media', 'doods.boo', 'doodx.pro', 'tem.pics', 'poop.cx', 'ds2video', 'd0o0d.com', 'poops.pink', 'doodf.com', 'poop.kim', 'poop.vin','do0od.com', 'do0d.com', 'doood.cam', 'd000d.com', 'd0000d.com']):
         return doods(link)
-    elif any(x in domain for x in ['streamtape.com', 'streamtape.co', 'streamtape.cc', 'streamtape.to', 'streamtape.net', 'streamta.pe', 'streamtape.xyz', 'streamnoads.com', 'tapenoads.com', 'tapeadsenjoyer.com']):
+    elif any(x in domain for x in ['streamtape.com', 'streamtape.co', 'streamtape.cc', 'streamtape.to', 'streamtape.net', 'streamta.pe', 'streamtape.xyz', 'streamnoads.com', 'tapenoads.com', 'tapeadsenjoyer.com', 'gettapeads.com']):
         return streamtape(link)
     elif any(x in domain for x in ['wetransfer.com', 'we.tl']):
         return wetransfer(link)
