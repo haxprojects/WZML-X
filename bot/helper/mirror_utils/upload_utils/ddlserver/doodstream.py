1f0b39c2cbc1f13275e8bcbd0b7cbbcd60fbc5b9
noobzhax <azorax@pm.me>
2024-05-12 19:35:36 +0700

    feat: add doodstream ddl

---
+++ bot/helper/mirror_utils/upload_utils/ddlserver/doodstream.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+from os import path as ospath, walk
+from aiofiles.os import path as aiopath, rename as aiorename
+from asyncio import sleep
+from aiohttp import ClientSession
+
+from bot import LOGGER
+from bot.helper.ext_utils.bot_utils import sync_to_async
+
+ALLOWED_EXTS = [
+	'.avi', '.mkv', '.mpg', '.mpeg', '.vob', '.wmv', '.flv', '.mp4', '.mov', '.m4v',
+	'.m2v', '.divx', '.3gp', '.webm', '.ogv', '.ogg', '.ts', '.ogm'
+]
+
+class Doodstream:
+	"""docstring for Doostream"""
+
+	def __init__(self, dluploader, api_key):
+		self.apiKey = api_key
+		self.dluploader = dluploader
+		self.base_url = "https://doodapi.com"
+
+	async def __resp_handler(self, response):
+		api_resp = response.get("msg", "")
+		if api_resp == "ok":
+			return response["result"]
+		raise Exception(api_resp.split("-")[1] if "error-" in api_resp else "Response Status is not ok and Reason is Unknown")
+
+
+	async def __getAccInfo(self):
+		async with ClientSession() as session, session.get(f"{self.base_url}/api/account/info?key={self.apiKey}") as response:
+			if response.status == 200:
+				if (data := await response.json()) and data["status"] == 200:
+					return data["result"]
+		return None
+
+	async def __getServer(self):
+		async with ClientSession() as session:
+			async with session.get(f"{self.base_url}/api/upload/server?key={self.apiKey}") as resp:
+				serv = await resp.json()
+				return serv['result']
+
+	async def upload_file(self, path: str):
+		server = (await self.__getServer())
+		LOGGER.info(f"Using: {server}")
+		apiKey = self.apiKey if self.apiKey else ""
+		req_dict = {}
+		if apiKey:
+			req_dict["api_key"] = apiKey
+
+		if self.dluploader.is_cancelled:
+			return
+		new_path = ospath.join(ospath.dirname(path), ospath.basename(path).replace(' ', '.'))
+		await aiorename(path, new_path)
+		self.dluploader.last_uploaded = 0
+		LOGGER.info(req_dict)
+		upload_file = await self.dluploader.upload_aiohttp(f"{server}", new_path, "file", req_dict)
+		LOGGER.info(upload_file)
+		return upload_file 
+
+	async def upload(self, file_path):
+		resl = None
+		if not await self.__getAccInfo():
+			raise Exception("Invalid doodstream API Key, Recheck your account !!")
+		if await aiopath.isfile(file_path):
+			gCode = await self.upload_file(path=file_path)
+			return gCode['result'][0]['download_url'] if gCode and 'result' in gCode and gCode['result'][0].get("download_url") else None
+		if self.dluploader.is_cancelled:
+			return
+		raise Exception("Failed to upload file/folder to doodstream API, Retry or Try after sometimes...")
--- bot/modules/users_settings.py
+++ bot/modules/users_settings.py
@@ -43,6 +43,7 @@ desp_dict = {'rcc': ['RClone is a command-line program to sync files and directo
             'user_tds': [f'UserTD helps to Upload files via Bot to your Custom Drive Destination via Global SA mail\n\n➲ <b>SA Mail :</b> {"Not Specified" if "USER_TD_SA" not in config_dict else config_dict["USER_TD_SA"]}', 'Send User TD details for Use while Mirror/Clone\n➲ <b>Format:</b>\nname id/link index(optional)\nname2 link2/id2 index(optional)\n\n<b>NOTE:</b>\n<i>1. Drive ID must be valid, then only it will accept\n2. Names can have spaces\n3. All UserTDs are updated on every change\n4. To delete specific UserTD, give Name(s) separated by each line</i>\n\n<b>Timeout:</b> 60 sec'],
             'gofile': ['Gofile is a free file sharing and storage platform. You can store and share your content without any limit.', "Send GoFile's API Key. Get it on https://gofile.io/myProfile, It will not be Accepted if the API Key is Invalid !!\n<b>Timeout:</b> 60 sec"],
             'streamtape': ['Streamtape is free Video Streaming & sharing Hoster', "Send StreamTape's Login and Key\n<b>Format:</b> <code>user_login:pass_key</code>\n<b>Timeout:</b> 60 sec"],
+            'doodstream': ['doodstream is a free file sharing and storage platform. You can store and share your content without any limit.', "Send doodstream's API Key. Get it on https://doodstream.com, It will not be Accepted if the API Key is Invalid !!\n<b>Timeout:</b> 60 sec"],
             }
 fname_dict = {'rcc': 'RClone',
              'lprefix': 'Prefix',
@@ -61,6 +62,7 @@ fname_dict = {'rcc': 'RClone',
              'user_tds': 'User Custom TDs',
              'gofile': 'GoFile',
              'streamtape': 'StreamTape',
+             'doodstream': 'Doodstream',
              }
 
 async def get_user_settings(from_user, key=None, edit_type=None, edit_mode=None):
@@ -190,7 +192,7 @@ async def get_user_settings(from_user, key=None, edit_type=None, edit_mode=None)
         text = f"㊂ <b><u>{fname_dict[key]} Settings :</u></b>\n\n" \
                f"➲ <b>Enabled DDL Server(s) :</b> <i>{ddl_serv}</i>\n\n" \
                f"➲ <b>Description :</b> <i>{desp_dict[key][0]}</i>"
-        for btn in ['gofile', 'streamtape']:
+        for btn in ['gofile', 'streamtape', 'doodstream']:
             buttons.ibutton(f"{'✅️' if btn in serv_list else ''} {fname_dict[btn]}", f"userset {user_id} {btn}")
         buttons.ibutton("Back", f"userset {user_id} back mirror", "footer")
         buttons.ibutton("Close", f"userset {user_id} close", "footer")
@@ -228,7 +230,7 @@ async def get_user_settings(from_user, key=None, edit_type=None, edit_mode=None)
         elif key in ['mprefix', 'mremname', 'msuffix']:
             set_exist = 'Not Exists' if (val:=user_dict.get(key, config_dict.get(f'MIRROR_FILENAME_{key[1:].upper()}', ''))) == '' else val
             text += f"➲ <b>Mirror Filename {fname_dict[key]} :</b> {set_exist}\n\n"
-        elif key in ['gofile', 'streamtape']:
+        elif key in ['gofile', 'streamtape', 'doodstream']:
             set_exist = 'Exists' if key in (ddl_dict:=user_dict.get('ddl_servers', {})) and ddl_dict[key][1] and ddl_dict[key][1] != '' else 'Not Exists'
             ddl_mode = 'Enabled' if key in (ddl_dict:=user_dict.get('ddl_servers', {})) and ddl_dict[key][0] else 'Disabled'
             text = f"➲ <b>Upload {fname_dict[key]} :</b> {ddl_mode}\n" \
@@ -309,7 +311,7 @@ async def set_custom(client, message, pre_event, key, direct=False):
     return_key = 'leech'
     n_key = key
     user_dict = user_data.get(user_id, {})
-    if key in ['gofile', 'streamtape']:
+    if key in ['gofile', 'streamtape', 'doodstream']:
         ddl_dict = user_dict.get('ddl_servers', {})
         mode, api = ddl_dict.get(key, [False, ""])
         if key == "gofile" and not await Gofile.is_goapi(value):
@@ -563,7 +565,7 @@ async def edit_user_settings(client, query):
         await update_user_settings(query, 'leech')
         if DATABASE_URL:
             await DbManger().update_user_data(user_id)
-    elif data[2] in ['sgofile', 'sstreamtape', 'dgofile', 'dstreamtape']:
+    elif data[2] in ['sgofile', 'sstreamtape', 'sdoodstream', 'dgofile', 'dstreamtape', 'ddoodstream']:
         handler_dict[user_id] = False
         ddl_dict = user_dict.get('ddl_servers', {})
         key = data[2][1:]
@@ -599,7 +601,7 @@ async def edit_user_settings(client, query):
         else:
             await query.answer("Old Settings", show_alert=True)
             await update_user_settings(query)
-    elif data[2] in ['ddl_servers', 'user_tds', 'gofile', 'streamtape']:
+    elif data[2] in ['ddl_servers', 'user_tds', 'gofile', 'streamtape', 'doodstream']:
         handler_dict[user_id] = False
         await query.answer()
         edit_mode = len(data) == 4
